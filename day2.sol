f8b9878b54fe7c7461a1e995183cc560290aac12
German Aquila <g.axl.aquila@gmail.com>
2021-05-24 12:47:31 -0300

    Create a contract for a real state property

---
+++ day2.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: GPL-3.0
+ 
+pragma solidity >=0.5.0 <0.9.0;
+ 
+contract Property{
+    // declaring state variables saved in contract's storage
+    uint price; // by default is private
+    string public location;
+    
+    // can be initialized at declaration or in the constructor only
+    address immutable public owner; 
+    
+    // declaring a constant
+    int constant area = 100;
+    
+    // declaring the constructor
+    // is executed only once at contract's deployment
+    constructor(uint _price, string memory _location){
+        price = _price;
+        location = _location;
+        owner = msg.sender;  // initializing owner to the account's address that deploys the contract
+    }
+    
